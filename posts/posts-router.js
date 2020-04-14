e1f0d7d47f5ddd3185fa67ae4eb64844326e2f84
Jane Suh <sourcherryyogurt@gmail.com>
2020-04-14 17:02:13 -0400

    Added DELETE and PUT requests

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -48,7 +48,7 @@ router.post('/:id/comments', (req, res) => {
 // This handles the route 'GET /posts'
 router.get('/', (req, res) => {
     try {
-        db.find();
+        res.json(db.find());
     } catch {
         res.status(500).json({
             message: 'The posts information could not be retrieved.'
@@ -91,6 +91,52 @@ router.get('/:id/comments', (req, res) => {
             message: 'The comments information could not be retrieved.'
         });
     }
+});
+
+// This handles the route 'DELETE /posts/:id'
+router.delete('/:id', (req, res) => {
+    try {
+        const post = db.findById(req.params.id);
+        if (post) {
+            db.findById(req.params.id);
+            res.status(200).json({
+                message: 'The post has been removed.'
+            });
+            db.remove(req.params.id);
+        } else {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist.'
+            });
+        }
+    } catch {
+        res.status(500).json({
+            message: 'The post could not be removed.'
+        });
+    }
+});
+
+// This handles the route 'PUT /posts/:id'
+router.put('/:id', (req, res) => {
+    try {
+        const post = db.findById(req.params.id);
+        if (!req.body.title || !req.body.contents) {
+            res.status(400).json({
+                message: 'Please provide title and contents for the post.'
+            });
+        }
+        if (post) {
+            db.update(req.params.id, req.body);
+            res.status(200).json(req.body);
+        } else {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist.'
+            });
+        }
+    } catch {
+        res.status(500).json({
+            message: 'The post information could not be modified.'
+        });
+    }  
 });
 
 module.exports = router;
